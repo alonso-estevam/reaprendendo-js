9d9a86cc430376062c230cc83abe7088114e12c8
alonso-estevam <alonso_estevam@outlook.com>
2023-05-07 21:15:15 -0300

    explicado tipos de saída

--- aula001/index.js
+++ aula001/index.js
@@ -1,3 +1,7 @@
 function alertaExterno() {
   alert("Já esse vem de um arquivo JS externo!");
 }
+
+document.getElementById(
+  "exemploInnerHtml"
+).innerHTML = `<em>Esse é um texto escrito com o innerHTML!</em>`;
